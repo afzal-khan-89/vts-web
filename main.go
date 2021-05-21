5ad14b680551a879a9a60e3f3b77f7514aef4b3b
attila <to.babu.khan@gmail.com>
2021-05-21 13:49:02 +0600

    nTc

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-func main(){
+	"github.com/afzal-khan-89/vts-web/routs"
+)
+
+func main() {
 	fmt.Println("hello go ")
+	routs.RoutTest()
 }
---
+++ routes/reports.go
@@ -0,0 +1,7 @@
+package routs
+
+import "fmt"
+
+func RoutTest() {
+	fmt.Println("rout package is working ....")
+}
